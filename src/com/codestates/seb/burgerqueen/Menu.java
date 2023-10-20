f5002638cfeb794d2b2d74305023088449f1975f
yoonseonjin <saltycreamlatte@gmail.com>
2023-10-21 02:37:19 +0900

    메서드 추출 리팩토링

--- src/com/codestates/seb/burgerqueen/Menu.java
+++ src/com/codestates/seb/burgerqueen/Menu.java
@@ -16,52 +16,54 @@ public class Menu {
 		System.out.println("메뉴");
 		System.out.println("-".repeat(60));
 
-		System.out.println("햄버거");
-		for (Product product : products) {
-			if (product instanceof Hamburger) {
-				System.out.printf(
-					"(%d) %s %5dKcal %5d원\n",
-					product.getId(),
-					product.getName(),
-					product.getKcal(),
-					product.getPrice()
-				);
-			}
-		}
-		System.out.println();
-
-		System.out.println("사이드");
-		for (Product product : products) {
-			if (product instanceof Side) {
-				System.out.printf(
-					"(%d) %s %5dKcal %5d원\n",
-					product.getId(),
-					product.getName(),
-					product.getKcal(),
-					product.getPrice()
-				);
-			}
-		}
-		System.out.println();
-
-		System.out.println("음료");
-		for (Product product : products) {
-			if (product instanceof Drink) {
-				System.out.printf(
-					"(%d) %s %5dKcal %5d원\n",
-					product.getId(),
-					product.getName(),
-					product.getKcal(),
-					product.getPrice()
-				);
-			}
-		}
-		System.out.println();
+		printHamburgers();
+		printSides();
+		printDrinks();
 
 		System.out.println();
 		System.out.println("(0) 장바구니");
 		System.out.println("(+) 주문하기");
 		System.out.println("-".repeat(60));
 		System.out.print("메뉴를 선택해 주세요 : ");
+	}
+
+	private void printDrinks() {
+		System.out.println("음료");
+		for (Product product : products) {
+			if (product instanceof Drink) {
+				printEachMenu(product);
+			}
+		}
+		System.out.println();
+	}
+
+	private void printSides() {
+		System.out.println("사이드");
+		for (Product product : products) {
+			if (product instanceof Side) {
+				printEachMenu(product);
+			}
+		}
+		System.out.println();
+	}
+
+	private void printHamburgers() {
+		System.out.println("햄버거");
+		for (Product product : products) {
+			if (product instanceof Hamburger) {
+				printEachMenu(product);
+			}
+		}
+		System.out.println();
+	}
+
+	private static void printEachMenu(Product product) {
+		System.out.printf(
+			"(%d) %s %5dKcal %5d원\n",
+			product.getId(),
+			product.getName(),
+			product.getKcal(),
+			product.getPrice()
+		);
 	}
 }
